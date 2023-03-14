a59173037a5f9e870f63b7bfc847cba55cd39ffc
Estanislao Maza <estanlee18@gmail.com>
2023-03-13 22:12:56 -0300

    inicio mi proyecto aca

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var WENSESLAO= "PEPE";
+console.log(WENSESLAO);
+/*alert(WENSESLAO)*/
+let n1= "10";
+let n2= parseFloat ("5");
+n1= parseFloat (n1)
+let resultado = n1 + n2;
+
+/*alert(resultado) */
+if(true){
+console.log("mensaje visible")
+};
+
+let estado=prompt("llueve afuera?")
+if(estado == "si"){
+    console.log("llevo paraguas")
+}else{
+    console.log("no llevo paraguas")
+}
